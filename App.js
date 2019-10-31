17e0f8cdf50d80f67267acab3cb5dba6c09f1d46
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2019-10-30 22:07:57 -0300

    Encadeamento

--- App.js
+++ App.js
@@ -24,13 +24,36 @@ const styles = StyleSheet.create({
 
 const App = () => {
   const [ballY, setBallY] = useState(new Animated.Value(0));
-  const [ballX, setBallX] = useState(Animated.divide(ballY, 2));
+  const [ballX, setBallX] = useState(new Animated.Value(0));
 
   useEffect(() => {
-    Animated.decay(ballY, {
-      velocity: 0.5,
-    }).start();
-  }, [ballY]);
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(ballY, {
+          toValue: 200,
+          duration: 500,
+        }),
+        Animated.delay(200),
+        Animated.timing(ballX, {
+          toValue: 200,
+          duration: 500,
+        }),
+        Animated.delay(200),
+        Animated.timing(ballY, {
+          toValue: 0,
+          duration: 500,
+        }),
+        Animated.delay(200),
+        Animated.timing(ballX, {
+          toValue: 0,
+          duration: 500,
+        }),
+      ]),
+      {
+        iterations: 2,
+      }
+    ).start();
+  }, [ballX, ballY]);
 
   return (
     <View style={styles.container}>
