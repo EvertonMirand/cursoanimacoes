965542a7bda557f95c74595b13b1ceb909e48632
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2019-10-30 21:48:22 -0300

    Animação timing

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Animated } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 30,
+  },
+  ball: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: '#f00',
+  },
+});
+
+const App = () => {
+  const [ballY, setBallY] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(ballY, {
+      toValue: 500,
+      duration: 1000,
+    }).start();
+  }, [ballY]);
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.ball, { top: ballY }]} />
+    </View>
+  );
+};
+
+export default App;
